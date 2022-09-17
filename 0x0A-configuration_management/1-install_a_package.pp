e2028a8d96afd552874ecef7ef3af8a938dbe848
Caleb Nyong <65078408+calebapril@users.noreply.github.com>
2022-09-17 13:18:57 +0100

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Installs a package
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
